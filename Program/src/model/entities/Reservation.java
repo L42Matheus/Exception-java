a4a63fbe0b79c68a3f6a476fb0717fe68e8f64f3
L42Matheus <lmatheus.tome@gmail.com>
2023-07-26 13:17:01 -0300

    feat: add method duration to make checkin

--- Program/src/model/entities/Reservation.java
+++ Program/src/model/entities/Reservation.java
@@ -1,6 +1,7 @@
 package model.entities;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Reservation {
 
@@ -29,5 +30,10 @@ public class Reservation {
     public Date getCheckout() {
         return checkout;
     }
-    
+
+    public long duration(){
+        long diff = checkin.getTime() - checkin.getTime();
+        //Converte milissegundo para dia
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
 }
