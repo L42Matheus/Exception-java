0b8ac95a3b68a81bd00f28a7b21805f885a4b9b1
L42Matheus <lmatheus.tome@gmail.com>
2023-07-26 13:36:12 -0300

    feat: add method to print with toString

--- Program/src/model/entities/Reservation.java
+++ Program/src/model/entities/Reservation.java
@@ -1,5 +1,6 @@
 package model.entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -8,6 +9,8 @@ public class Reservation {
     private Integer roomNumber;
     private Date checkin;
     private Date checkout;
+
+    public static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
     public Reservation(Integer roomNumber, Date checkin, Date checkout) {
         this.roomNumber = roomNumber;
@@ -40,5 +43,18 @@ public class Reservation {
     public void updateDates(Date checkin, Date checkout){
         this.checkin = checkin;
         this.checkout = checkout;
+    }
+
+    @Override
+    public String toString() {
+        return "Room "
+                + roomNumber
+                +", check-in:"
+                + simpleDateFormat.format(checkin)
+                + ", check-out: "
+                + simpleDateFormat.format(checkout)
+                + ", "
+                + duration()
+                + "nigths";
     }
 }
